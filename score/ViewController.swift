7a833b4e40de094f18702c9aec4f2a528c4129fa
vincenthng <Vincenthng@114-212-134-109.nju.edu.cn>
2016-06-07 14:41:00 +0800

    temp1

--- score/ViewController.swift
+++ score/ViewController.swift
@@ -13,6 +13,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let url:NSURL=NSURL(string:"http://score.ynzs.cn/gktk20163/checkimage.php")!
+        let data:NSData=NSData(contentsOfURL: url)!
+        let image=UIImage(data:data,scale:1.0)
+        let imageview:UIImageView=UIImageView(image:image)
+        let frame=CGRectMake(100, 320, 60, 20)
+        imageview.frame=frame
+        self.view.addSubview(imageview)
+        print(data)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +28,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBOutlet weak var Reset: UIButton!
 
+    @IBAction func ReSet(sender: UIButton) {
+        self.viewDidLoad()
+    }
 }
 
